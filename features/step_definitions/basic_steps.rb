fa48e2a21e7477bcb2bbc785e9e73e46b7931ca3
rinbo <robin.b@outlook.com>
2018-09-25 20:42:31 +0200

    Add feature file with tests and fix up controller to load and permit products

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,7 +1,19 @@
+
+Given("the following products exists") do |table|
+  table.hashes.each do |product|
+    Product.create(product)  
+end
+
+end
+
 Given("I visit the landing page") do
     visit root_path
   end
   
-  Then("I should see {string}") do |content|
-    expect(page).to have_content content
-  end
+Then("I should see {string}") do |content|
+  expect(page).to have_content content
+end  
+
+Given("I visit the product page") do
+  visit products_path
+end
