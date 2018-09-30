c79a77b7e9f4ea60dfc0c9a9feb3247638fb0b46
Miriam_Sallnäs <miriamfly54@hotmail.com>
2018-09-30 12:24:52 +0200

    remove white space

--- db/schema.rb
+++ db/schema.rb
@@ -89,7 +89,7 @@ ActiveRecord::Schema.define(version: 2018_09_27_145901) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.integer "price"
+    t.money "price", scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id"
