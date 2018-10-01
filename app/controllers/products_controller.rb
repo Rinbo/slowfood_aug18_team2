1ddc438302b887632a7803563fb8441addd40608
rinbo <robin.b@outlook.com>
2018-10-01 09:36:33 +0200

    Add styling to product page and make additions/removals to/from stylesheets

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,12 @@
 class ProductsController < ApplicationController
+  include ImageHelper
   def index
     @products = Product.all
+    @categories = Category.all
+    @starter = Category.find_by(name: "Starter")
+    @main = Category.find_by(name: "Main")
+    @dessert = Category.find_by(name: "Dessert")
+    @beverage = Category.find_by(name: "Beverage")    
   end
 
   def show
